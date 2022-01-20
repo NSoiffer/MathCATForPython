74b950bddfb49ad4e6333b9f0aac08c430769e5e
NSoiffer <NSoiffer@users.noreply.github.com>
2022-01-20 15:43:58 -0800

    Initial commit for NVDA plugin

---
+++ NVDA-addon/build.sh
@@ -0,0 +1,14 @@
+#!/bin/csh
+
+cargo build --release
+
+# copy over all the rules and then remove a few "extra" files
+cp -r ../../MathCAT/Rules addon/globalPlugins/
+rm -rf addon/globalPlugins/Rules/.tmp.driveupload
+rm -f addon/globalPlugins/Rules/Nemeth/unicode.yaml-with-all
+rm -rf addon/globalPlugins/Rules/zz
+
+cp ../target/i686-pc-windows-msvc/release/libmathcat_py.dll addon/globalPlugins/libmathcat.pyd
+rm mathCAT-0.1.1.nvda-addon
+scons
+
