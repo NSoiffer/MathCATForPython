52b2cd73d0e01b607f78ba73c6f1e542566cbf57
NSoiffer <NSoiffer@users.noreply.github.com>
2022-02-16 13:53:31 -0800

    Change to using Rules dir from the cargo download cache. This still wrong in that it hard codes in the MathCAT version. I need to find a way to get the current package version being used. Note: all the old versions of MathCAT are kept in the cache, so I can't use a wildcard ("*").

--- NVDA-addon/build.sh
+++ NVDA-addon/build.sh
@@ -3,8 +3,8 @@
 cargo build --target i686-pc-windows-msvc --release
 
 # copy over all the rules and then remove a few "extra" files
-cp -r ../../MathCAT/Rules addon/globalPlugins/MathCAT
-rm -rf addon/globalPlugins/MathCAT/Rules/.tmp.driveupload
+# FIX: need a way to keep using the most current version of MathCAT's rules
+cp -r ~/.cargo/registry/src/*/mathcat-0.1.5/Rules addon/globalPlugins/MathCAT
 rm -f addon/globalPlugins/MathCAT/Rules/Nemeth/unicode.yaml-with-all
 rm -rf addon/globalPlugins/MathCAT/Rules/zz
 
