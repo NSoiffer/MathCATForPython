e501579819be965636c366f8e7f02b5ab81e0039
NSoiffer <NSoiffer@users.noreply.github.com>
2022-03-23 13:52:38 -0700

    Some cleanup on building Addresses #16
    Added sed command to make import wx.xrc a comment

--- build-nvda-addon.sh
+++ build-nvda-addon.sh
@@ -1,10 +1,11 @@
 #!/bin/csh
 # RUN THIS FILE to build the NVDA addon
 
+rm -rf NVDA-addon/addon/globalPlugins/MathCAT/Rules
 cargo build --target i686-pc-windows-msvc --release
 
 cp target/i686-pc-windows-msvc/release/libmathcat_py.dll NVDA-addon/addon/globalPlugins/MathCAT/libmathcat.pyd
 cd NVDA-addon
+sed 's/^import wx\.xrc/# import wx.xrc/' --in-place "addon/globalPlugins/MathCAT/MathCATgui.py"
 rm MathCAT-*.nvda-addon
 scons
-
--- build.rs
+++ build.rs
@@ -1,15 +1,12 @@
 //! The build.rs file is necessary to unzip the rules.
 //! rules.zip are needed so there is a way to get the rules dir into the build since you can't get it from the crate.
 //!
-//! Note: this only creates the Rules dir. Run build-addon.sh to actually build the addon.
+//! Note: this only creates the Rules dir. Run build-addon.sh to actually build the NVDA addon.
 
 use std::path::PathBuf;
 use zip::ZipArchive;
 
 fn main() {
-    // first remove the Rules directory
-    std::fs::remove_dir_all("NVDA-addon/addon/globalPlugins/MathCAT/Rules")
-        .expect("Failed to remove directory 'NVDA-addon/addon/globalPlugins/MathCAT/Rules'");
     let archive = libmathcat::ZIPPED_RULE_FILES;
     let archive = std::io::Cursor::new(archive);
     let location = PathBuf::from("NVDA-addon/addon/globalPlugins/MathCAT");
@@ -18,6 +15,8 @@ fn main() {
     zip_archive.extract(&location).expect("Zip extraction failed");
 
     // the test dir 'zz' doesn't need to be part of the addon
-    std::fs::remove_dir_all("NVDA-addon/addon/globalPlugins/MathCAT/Rules/Languages/zz")
-        .expect("Failed to remove directory 'NVDA-addon/addon/globalPlugins/MathCAT/Rules/zz'");
+    let mut zz_dir = location.clone();
+    zz_dir.push("Rules/Languages/zz");
+    std::fs::remove_dir_all(&zz_dir)
+        .expect(&format!("Failed to remove directory {}", zz_dir.to_str().unwrap()));
 }
