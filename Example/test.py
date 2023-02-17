05815b1975286592ef7f4cfd975702d923ecfec6
NSoiffer <NSoiffer@users.noreply.github.com>
2023-02-17 00:23:38 +0000

    Sample code on how to use MathCAT

---
+++ Example/test.py
@@ -0,0 +1,58 @@
+# MathCAT add-on: generates speech, braille, and allows exploration of expressions written in MathML
+# The goal of this add-on is to replicate/improve on the functionality of MathPlayer which has been discontinued.
+# Author: Neil Soiffer
+# Copyright: this file uses whatever GNU copyright that is required for NVDA addons
+#   The code additionally makes use of the MathCAT library (written in Rust) which is covered by the MIT license
+#   and also (obviously) requires external speech engines and braille drivers.
+
+
+import os
+
+# import shutil
+# if os.path.exists("libmathcat_py.pyd"):
+# 	os.remove("libmathcat_py.pyd")
+# shutil.copy("..\\target\\i686-pc-windows-msvc\\release\\libmathcat_py.dll", "libmathcat.pyd")
+
+import libmathcat
+
+def SetMathCATPreferences():
+  try:
+    libmathcat.SetRulesDir(
+      # this assumes the Rules dir is in the same dir a the library. Modify as needed
+      os.path.join( os.path.dirname(os.path.abspath(__file__)), "Rules")
+    )
+  except Exception as e:
+    print("problem with finding the MathCAT rules")
+
+  try:
+    libmathcat.SetPreference("TTS", "none")
+    libmathcat.SetPreference("Language", "en")         # Also "id" and "vi"
+    libmathcat.SetPreference("SpeechStyle", "SimpleSpeak")   # Also "ClearSpeak"
+    libmathcat.SetPreference("Verbosity", "verbose")   # also terse "terse"/"medium"
+    libmathcat.SetPreference("CapitalLetters_UseWord", "false")   # if "true", X => "cap x"
+  except Exception as e:
+      print("problem with setting a preference")
+
+def SetMathMLForMathCAT(mathml: str):
+  try:
+    libmathcat.SetMathML(mathml)
+  except Exception as e:
+    print("problem with SetMathML")
+
+def GetSpeech():
+  try:
+    return libmathcat.GetSpokenText()
+  except Exception as e:
+    return "problem with getting speech for MathML"
+
+
+
+SetMathCATPreferences()   # you only need to this once
+
+mathml = "<math><mfrac> <mn>1</mn> <mi>X</mi> </mfrac> </math>"
+SetMathMLForMathCAT(mathml)
+print("MathML: {}\nSpeech: '{}'".format(mathml, GetSpeech()))
+
+mathml = "<math><msup> <mi>x</mi> <mn>3</mn> </msup> </math>"
+SetMathMLForMathCAT(mathml)
+print("MathML: {}\nSpeech: '{}'".format(mathml, GetSpeech()))
