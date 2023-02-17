6d0d4030207344636f4e1065114902e05a0e8278
NSoiffer <NSoiffer@users.noreply.github.com>
2023-02-17 00:13:08 +0000

    make explicit the PY03_PYTHON exe to use rather than having it hidden in some environment var

--- build-nvda-addon.sh
+++ build-nvda-addon.sh
@@ -2,8 +2,10 @@
 # RUN THIS FILE to build the NVDA addon
 
 rm -rf NVDA-addon/addon/globalPlugins/MathCAT/Rules
-cargo build --target i686-pc-windows-msvc --release
-
+# NVDA is currently uses 32 bit python 3.7
+# We need to tell PYO3 that
+set PYO3_PYTHON_32=c:/Users/neils/AppData/Local/Programs/Python/Python37-32/python.exe
+env PYO3_PYTHON=$PYO3_PYTHON_32 cargo build --target i686-pc-windows-msvc --release
 cp target/i686-pc-windows-msvc/release/libmathcat_py.dll NVDA-addon/addon/globalPlugins/MathCAT/libmathcat.pyd
 cd NVDA-addon
 sed 's/^import wx\.xrc/# import wx.xrc/' --in-place "addon/globalPlugins/MathCAT/MathCATgui.py"
