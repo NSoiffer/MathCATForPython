62e1f9b19c6c9a4c0c6fef14b0dae33a34cb182f
NSoiffer <NSoiffer@users.noreply.github.com>
2023-02-25 23:37:32 +0000

    made it more obvious what the 64bit python is located *on my main build machine*

--- build-nvda-addon.sh
+++ build-nvda-addon.sh
@@ -4,6 +4,7 @@
 rm -rf NVDA-addon/addon/globalPlugins/MathCAT/Rules
 # NVDA is currently uses 32 bit python 3.7
 # We need to tell PYO3 that
+set PYO3_PYTHON_64=c:/Users/neils/AppData/Local/Programs/Python/Python39/python.exe
 set PYO3_PYTHON_32=c:/Users/neils/AppData/Local/Programs/Python/Python37-32/python.exe
 env PYO3_PYTHON=$PYO3_PYTHON_32 cargo build --target i686-pc-windows-msvc --release
 cp target/i686-pc-windows-msvc/release/libmathcat_py.dll NVDA-addon/addon/globalPlugins/MathCAT/libmathcat.pyd
