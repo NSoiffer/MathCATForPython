d71c6a4849124d5dd9e34dfcfaa4dfd5b8e9a173
NSoiffer <NSoiffer@users.noreply.github.com>
2023-12-20 05:34:43 +0000

    fix up some comments

--- build-nvda-addon.sh
+++ build-nvda-addon.sh
@@ -2,7 +2,7 @@
 # RUN THIS FILE to build the NVDA addon
 
 rm -rf addon/globalPlugins/MathCAT/Rules
-# NVDA is currently uses 32 bit python 3.7
+# NVDA currently uses 32 bit python 3.7
 # We need to tell PYO3 that
 set PYO3_PYTHON_64=c:/Users/neils/AppData/Local/Programs/Python/Python39
 set PYO3_PYTHON_32=c:/Users/neils/AppData/Local/Programs/Python/Python37-32
@@ -10,4 +10,6 @@ env PYO3_PYTHON=$PYO3_PYTHON_32/python.exe cargo build --target i686-pc-windows-
 cp target/i686-pc-windows-msvc/release/libmathcat_py.dll addon/globalPlugins/MathCAT/libmathcat.pyd
 sed 's/^import wx\.xrc/# import wx.xrc/' --in-place "addon/globalPlugins/MathCAT/MathCATgui.py"
 rm MathCAT-*.nvda-addon
+
+# use "pip install SCons" to add scons to the python env
 $PYO3_PYTHON_32/Scripts/scons
