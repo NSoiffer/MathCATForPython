32f9c96f735518058f1590acf6a198625d4fbfb0
NSoiffer <NSoiffer@users.noreply.github.com>
2024-11-03 11:39:37 -0800

    update version #

--- buildVars.py
+++ buildVars.py
@@ -32,7 +32,7 @@ addon_info = {
         and other translations are in progress."""
     ),
     # version
-    "addon_version": "0.6.3",
+    "addon_version": "0.6.5",
     # Author(s)
     "addon_author": "Neil Soiffer <soiffer@alum.mit.edu>",
     # URL for the add-on documentation support
@@ -44,7 +44,7 @@ addon_info = {
     # Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
     "addon_minimumNVDAVersion": "2024.1",
     # Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-    "addon_lastTestedNVDAVersion": "2024.1",
+    "addon_lastTestedNVDAVersion": "2024.4",
     # Add-on update channel (default is None, denoting stable releases,
     # and for development releases, use "dev".)
     # Do not change unless you know what you are doing!
