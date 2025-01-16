d7599ec68512ca0439c755b64748bef298affdf3
NSoiffer <NSoiffer@users.noreply.github.com>
2025-01-15 17:32:05 -0800

    bump version number

--- buildVars.py
+++ buildVars.py
@@ -32,7 +32,7 @@ addon_info = {
         and other translations are in progress."""
     ),
     # version
-    "addon_version": "0.6.7",
+    "addon_version": "0.6.7-rc.3",
     # Author(s)
     "addon_author": "Neil Soiffer <soiffer@alum.mit.edu>",
     # URL for the add-on documentation support
